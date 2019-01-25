3ceacf638861fb85a8cf9fcb7a228497fda053b0
Lukas Herman <lherman.cs@gmail.com>
2019-01-24 20:06:48 -0500

    Added youtube-dl auto update

--- player/extractor.go
+++ player/extractor.go
@@ -3,7 +3,17 @@ package player
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
+)
+
+var (
+	ytdlPath string
 )
 
 // VideoInfo is a data structure extracted info from the given url
@@ -18,7 +28,7 @@ func extract(url string) (*VideoInfo, error) {
 	var buff bytes.Buffer
 	var info VideoInfo
 
-	cmd := exec.Command("youtube-dl", "-fbest", "-j", url)
+	cmd := exec.Command("python", ytdlPath, "-fbest", "-j", url)
 	cmd.Stdout = &buff
 	if err := cmd.Run(); err != nil {
 		return nil, err
@@ -28,4 +38,34 @@ func extract(url string) (*VideoInfo, error) {
 		return nil, err
 	}
 	return &info, nil
+}
+
+func init() {
+	fmt.Println("[extractor] updating youtube-dl")
+	curDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal("[extractor]", curDir)
+	}
+
+	ytdlPath = filepath.Join(curDir, "youtube-dl")
+
+	// check youtube-dl
+	resp, err := http.Get("https://yt-dl.org/downloads/latest/youtube-dl")
+	if err != nil {
+		log.Fatal("[extractor] failed to update youtube-dl")
+	}
+	defer resp.Body.Close()
+
+	f, err := os.Create(ytdlPath)
+	if err != nil {
+		log.Fatal("[extractor]", err)
+	}
+	defer f.Close()
+
+	_, err = io.CopyBuffer(f, resp.Body, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("[extractor] updated youtube-dl")
 }
